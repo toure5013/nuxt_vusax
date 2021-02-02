83a1ce85d310910686a7a120b4a7befcd52e7aaf
SOULEYMANE TOURE <toure5013@gmail.com>
2021-02-02 10:34:29 +0000

    add navbar and loading

---
+++ components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <vs-navbar  text-white color="dark" center-collapsed v-model="active">
+      <template #left>
+        <img src="../assets/logo_orange.png" alt="" class="logo">
+      </template>
+      <vs-navbar-item  id="guide">
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item  id="docs">
+        <NuxtLink to="/">Docs</NuxtLink>
+      </vs-navbar-item>
+      <template #right>
+        <vs-button>Login</vs-button>
+        <vs-button>Signup</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Appheader",
+ data() {
+     return {
+         active: "guide",
+     }
+ },
+};
+</script>
+
+<style>
+    .logo{
+        width : 15%;
+        height : 20%;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="hide-scroll-content" class="square">
+    <div class="child">child 1</div>
+    <div class="child">child 2</div>
+    <div class="child">child 3</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+      this.openLoading();
+  },
+  components: {},
+  methods: {
+    openLoading() {
+      const loading = this.$vs.loading({
+        type : "corners"
+      });
+      this.hasOpenLoading = true;
+      setTimeout(() => {
+        loading.close();
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style></style>
